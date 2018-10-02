f52c499f01b6b3669ec284003fb0c7d84ebf21be
Joey Mandarino <joey@leotta.com>
2018-10-02 14:47:05 -0400

    created a base model

---
+++ patchnotes/models.py
@@ -0,0 +1,9 @@
+import uuid
+from django.db import models
+
+class BaseModel(models.Model):
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False)
+    updated_at = models.DateTimeField(auto_now=True, editable=False)
+
+    class Meta:
+        abstract = True
